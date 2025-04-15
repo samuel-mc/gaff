1bc408121cc4fdc45c72271dade0dc015f09e2f9
Samuel Medina <smdoomed@gmail.com>
2025-04-14 20:54:52 -0600

    Add all changes

---
+++ views/partials/footer.php
@@ -0,0 +1,195 @@
+<footer class="bg--black px-4 py-4">
+    <div class="row m-0 p-0">
+        <!-- left content -->
+        <div class="col-lg-4 p-0 m-0 py-5">
+            <div class="text-end mb-4">
+                <img src="<?php echo __ROOT__; ?>/public/img/footer.png" alt="footer logo" width="243" height="117.19">
+            </div>
+            <p class="poppins-regular text--sm text-white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada arcu a est laoreet, ut consectetur diam tempor. Etiam elementum tristique aliquam. 
+            </p>
+        </div>
+        <!-- right content -->
+        <div class="col-lg-8 row p-0 m-0 px-2 px-md-5 py-4" id="footerLinks">
+            <div class="my-1 col-12 col-sm-6 col-md-4 col-lg-2">
+                <ul class="list-unstyled poppins-regular text-white text--sm">
+                    <li class="mb-3">
+                        <a href="<?php echo __ROOT__; ?>/products/suspension" class="a--orange poppins-semibold">Suspensión</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/barra" class="a--white">Barra</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/buje" class="a--white">Buje</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/cojinete" class="a--white">Cojinete</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/componente" class="a--white">Componente</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/kit" class="a--white">Kit</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/soporte" class="a--white">Soporte</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/tope" class="a--white">Tope</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="my-1 col-12 col-sm-6 col-md-4 col-lg-2">
+                <ul class="list-unstyled poppins-regular text-white text--sm">
+                    <li class="mb-3">
+                        <a href="<?php echo __ROOT__; ?>/products/cofre" class="a--orange poppins-semibold">Cofre</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/buje" class="a--white">Buje</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/gancho" class="a--white">Gancho y Base</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/kit" class="a--white">Kit</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/soporte" class="a--white">Soporte</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/tope" class="a--white">Tope</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="my-1 col-12 col-sm-6 col-md-4 col-lg-2">
+                <ul class="list-unstyled poppins-regular text-white text--sm">
+                    <li class="mb-3">
+                        <a href="<?php echo __ROOT__; ?>/products/cabina" class="a--orange poppins-semibold">Cabina</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/barra" class="a--white">Barra</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/buje" class="a--white">Buje</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/kit" class="a--white">Kit</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/soporte" class="a--white">Soporte</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="my-1 col-12 col-sm-6 col-md-4 col-lg-2">
+                <ul class="list-unstyled poppins-regular text-white text--sm">
+                    <li class="mb-3">
+                        <a href="<?php echo __ROOT__; ?>/products/quinta" class="a--orange poppins-semibold">Quinta Rueda</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/buje" class="a--white">Buje</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/kit" class="a--white">Kit</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/soporte" class="a--white">Soporte</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="my-1 col-12 col-sm-6 col-md-4 col-lg-4">
+                <ul class="list-unstyled poppins-regular text-white text--sm">
+                    <li class="mb-3">
+                        <a href="<?php echo __ROOT__; ?>/products/motor" class="a--orange poppins-semibold">Motor</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/empaque" class="a--white">Empaque</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/kit" class="a--white">Kit</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/soporte" class="a--white">Soporte</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="my-1 col-12 col-sm-6 col-md-4 col-lg-2">
+                <ul class="list-unstyled poppins-regular text-white text--sm">
+                    <li class="mb-3">
+                        <a href="<?php echo __ROOT__; ?>/products/frenos" class="a--orange poppins-semibold">Frenos</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/buje" class="a--white">Buje</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/empaque" class="a--white">Empaque</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/kit" class="a--white">Kit</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/rodillo" class="a--white">Rodillo</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="my-1 col-12 col-sm-6 col-md-4 col-lg-2">
+                <ul class="list-unstyled poppins-regular text-white text--sm">
+                    <li class="mb-3">
+                        <a href="<?php echo __ROOT__; ?>/products/seguridad" class="a--orange poppins-semibold">Seguridad</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/banderin" class="a--white">Banderín</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/cojinete" class="a--white">Cojinete</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/componente" class="a--white">Componente</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/soporte" class="a--white">Soporte</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/tope" class="a--white">Tope</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="my-1 col-12 col-sm-6 col-md-4 col-lg-2">
+                <ul class="list-unstyled poppins-regular text-white text--sm">
+                    <li class="mb-3">
+                        <a href="<?php echo __ROOT__; ?>/products/temperatura" class="a--orange poppins-semibold">Temperatura</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/buje" class="a--white">Buje</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/kit" class="a--white">Kit</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/soporte" class="a--white">Soporte</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="my-1 col-12 col-sm-6 col-md-4 col-lg-2">
+                <ul class="list-unstyled poppins-regular text-white text--sm">
+                    <li class="mb-3">
+                        <a href="<?php echo __ROOT__; ?>/products/transmision" class="a--orange poppins-semibold">Transmisión</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/buje" class="a--white">Buje</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="<?php echo __ROOT__; ?>/products/soporte" class="a--white">Soporte</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+<script src="<?php echo __ROOT__; ?>/public/js/index.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="<?php echo __ROOT__; ?>/public/lib/bootstrap-5.3.5-dist/js/bootstrap.min.js"></script>
+
+</html>
---
+++ views/partials/navbar.php
@@ -0,0 +1,112 @@
+<?php $title = isset($title) ? $title : 'Gaff';
+?>
+
+<header>
+    <!--  top part  -->
+    <div class="px-4 py-2 bg--black d-flex flex-column flex-md-row justify-content-between align-items-center">
+        <div class="">
+            <ul class="list-unstyled d-flex m-0">
+                <li class="mx-2">
+                    <a class="a--cyan" href="#">
+                        <i class="fa-brands fa-square-facebook fa-xl"></i>
+                    </a>
+                </li>
+                <li class="mx-2">
+                    <a class="a--cyan" href="#">
+                        <i class="fa-brands fa-square-instagram fa-xl"></i>
+                    </a>
+                </li>
+                <li class="mx-2">
+                    <a class="a--cyan" href="#">
+                        <i class="fa-brands fa-linkedin fa-xl"></i>
+                    </a>
+                </li>
+                <li class="mx-2">
+                    <a class="a--cyan" href="#">
+                        <i class="fa-brands fa-twitter fa-xl"></i>
+                    </a>
+                </li>
+                <li class="mx-2">
+                    <a class="a--cyan" href="#">
+                        <i class="fa-brands fa-tumblr fa-xl"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <button type="button" class="btn btn-transparent d-flex align-items-center text-white py-3">
+                <span style="min-width: max-content; margin-right: 8px" class="poppins-regular">Cambiar región</span>
+                <img src="<?php echo __ROOT__; ?>/public/img/mexicoFlag.png" alt="Bandera de mexico">
+            </button>
+        </div>
+    </div>
+    <!-- bottom part - desktop -->
+    <div class="d-none d-lg-flex justify-content-between align-items-center py-4 px-5">
+        <!-- left side -->
+        <div>
+            <button type="button" class="btn btn-transparent p-0 p-2 a--black">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+
+        <!-- center -->
+        <div>
+            <ul class="list-unstyled d-flex poppins-regular">
+                <li class="mx-3">
+                    <a class="a--black" href="<?php echo __ROOT__; ?>/"> Inicio</a>
+                </li>
+                <li class="mx-3">
+                    <a class="a--black" href="<?php echo __ROOT__; ?>/products"> Productos</a>
+                </li>
+                <li class="mx-3">
+                    <a class="a--black" href="<?php echo __ROOT__; ?>/about"> Acerca de nosotros</a>
+                </li>
+                <li class="mx-3">
+                    <a class="a--black" href="<?php echo __ROOT__; ?>/catalog"> Catálogo de productos</a>
+                </li>
+                <li class="mx-3">
+                    <a class="a--black" href="<?php echo __ROOT__; ?>/distributors"> Distribuidores</a>
+                </li>
+                <li class="mx-3">
+                    <a class="a--black" href="<?php echo __ROOT__; ?>/news"> Entérate</a>
+                </li>
+                <li class="mx-3">
+                    <a class="a--black" href="<?php echo __ROOT__; ?>/contact"> Contacto</a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- right side -->
+        <div>
+            <a href="<?php echo __ROOT__; ?>/">
+                <img src="<?php echo __ROOT__; ?>/public/img/logo.png" alt="logo" width="69.7" height="66.63">
+            </a>
+        </div>
+    </div>
+    <!-- bottom part - mobile -->
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="<?php echo __ROOT__; ?>/public/img/logo.png" alt="logo" width="69.7" height="66.63">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav py-4">
+                        <a class="nav-link a--black my-1" href="<?php echo __ROOT__; ?>/">Inicio</a>
+                        <a class="nav-link a--black my-1" href="<?php echo __ROOT__; ?>/products">Productos</a>
+                        <a class="nav-link a--black my-1" href="<?php echo __ROOT__; ?>/about">Acerca de nosotros</a>
+                        <a class="nav-link a--black my-1" href="<?php echo __ROOT__; ?>/catalog">Catálogo de productos</a>
+                        <a class="nav-link a--black my-1" href="<?php echo __ROOT__; ?>/distributors">Distribuidores</a>
+                        <a class="nav-link a--black my-1" href="<?php echo __ROOT__; ?>/news">Entérate</a>
+                        <a class="nav-link a--black my-1" href="<?php echo __ROOT__; ?>/contact">Contacto</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
